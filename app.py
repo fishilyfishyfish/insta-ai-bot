df65d73af8aeaf9ac06bdac887975aa745d61eea
fishilyfishyfish <namrednals@gmail.com>
2026-01-01 14:11:43 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "insta_ai_verify"
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Forbidden", 403
+
+    if request.method == "POST":
+        data = request.json
+        print("INSTAGRAM EVENT:", data)
+        return "EVENT_RECEIVED", 200
+
+@app.route("/")
+def home():
+    return "Server is running"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
